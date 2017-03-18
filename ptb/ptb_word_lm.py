053990d9ff3286f6a50350571a8de033e3dc6c26
ChenFuhua <543518975@qq.com>
2017-03-18 14:18:28 +0800

    Add run_iter

--- ptb/ptb_word_lm.py
+++ ptb/ptb_word_lm.py
@@ -14,7 +14,7 @@ flags = tf.flags
 logging = tf.logging
 
 flags.DEFINE_string(
-    "model", "large",
+    "model", "medium",
     "A type of model. Possible options are: small, medium, large.")
 flags.DEFINE_string("data_path", '../data', "data_path")
 flags.DEFINE_bool("use_fp16", False,
@@ -201,6 +201,22 @@ def get_config():
         raise ValueError("Invalid model: %s", FLAGS.model)
 
 
+@exe_time
+def run_iter(session, m, data, eval_op, x, y, state, verbose, step,
+             epoch_size, costs, iters, start_time):
+    cost, state, _ = session.run([m.cost, m.final_state, eval_op],
+                                 feed_dict={m.input_data: x,
+                                            m.targets: y,
+                                            m.initial_state: state})
+    costs += cost
+    iters += m.num_steps
+    if verbose and step % (epoch_size // 10) == 10:
+        print("%.3f perplexity: %.3f speed: %.0f wps" %
+              (step * 1.0 / epoch_size, np.exp(costs / iters),
+               iters * m.batch_size / (time.time() - start_time)))
+    return costs, iters
+
+
 def run_epoch(session, m, data, eval_op, verbose=False):
     epoch_size = (len(data) // m.batch_size - 1) // m.num_steps
     start_time = time.time()
@@ -209,17 +225,8 @@ def run_epoch(session, m, data, eval_op, verbose=False):
     state = session.run(fetches=m.initial_state)
     for step, (x, y) in enumerate(reader.ptb_iterator(data, m.batch_size,
                                                       m.num_steps)):
-        cost, state, _ = session.run([m.cost, m.final_state, eval_op],
-                                     feed_dict={m.input_data: x,
-                                                m.targets: y,
-                                                m.initial_state: state})
-        costs += cost
-        iters += m.num_steps
-
-        if verbose and step % (epoch_size // 10) == 10:
-            print("%.3f perplexity: %.3f speed: %.0f wps" %
-                  (step * 1.0 / epoch_size, np.exp(costs / iters),
-                   iters * m.batch_size / (time.time() - start_time)))
+        costs, iters = run_iter(session, m, data, eval_op, x, y, state, verbose, step,
+                                epoch_size, costs, iters, start_time)
     return np.exp(costs / iters)
 
 
